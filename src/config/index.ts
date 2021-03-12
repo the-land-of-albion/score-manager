2eeb75db7401b7b7cc3aaf0ed9cb3ccb16202c4e
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-03-12 09:09:02 +0100

    renameing

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,8 @@ const prefix = NODE_ENV == "production" ? "!sb" :  "?sb";
 export default {
     bot: {
         prefix,
-        name: "Captain. Scoreboard",
-        iconURL: "https://raw.githubusercontent.com/BotHaven/static/main/img/cpt_scoreboard.png"
+        name: "Lord Helyes",
+        iconURL: "https://raw.githubusercontent.com/BotHaven/static/main/img/three-wise-men/gaspar.png"
     },
     api: {
         prefix: "http://localhost:3000"
