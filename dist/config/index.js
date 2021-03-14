3c05e32cc3cff386ae900ebec32873bb9efe9a9d
Robert Bornschein <Robert.bornschein@mailbox.tu-dresden.de>
2021-03-14 07:17:41 +0100

    Delete dist directory

--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    bot: {
-        prefix: "!sb",
-        name: "Captain. Scoreboard"
-    }
-};
-//# sourceMappingURL=index.js.map
